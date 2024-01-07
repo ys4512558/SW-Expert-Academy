1d05feb332a89e13cca494b3e056cbded0ba7682
beudicri <ys4512558@naver.com>
2024-01-07 23:10:41 +0900

    feat: SWEA 알고리즘 Track(난이도 상) 1868 해결

--- Algorithm_high/src/Solution.java
+++ Algorithm_high/src/Solution.java
@@ -11,6 +11,124 @@ public class Solution {
     }
 
     /**
+     * SWEA: 1868. 파빙파빙 지뢰찾기
+     * @throws IOException
+     */
+
+    private static void prob1868() throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= T; i++) {
+            sb.append("#" + i + " ");
+            sb.append(solve1868()).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static int solve1868() throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        char map[][] = new char[N][N];
+        int res[][] = new int[N][N];
+
+        boolean visit[][] = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < N; j++) {
+                char c = input.charAt(j);
+                if(c == '*'){
+                    visit[i][j] = true;
+                }
+                map[i][j] = c;
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == '*') {
+                    res[i][j] = -1;
+                } else{
+                    res[i][j] = calcArea(i, j, map);
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (res[i][j] == 0 && !visit[i][j]) {
+                    visit = bfs1868(i, j, res, visit);
+                    cnt++;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visit[i][j]) {
+                    cnt++;
+                }
+            }
+        }
+
+
+        return cnt;
+    }
+
+    private static boolean[][] bfs1868(int row, int col, int[][] res, boolean[][] visit) {
+        int dy[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int dx[] = {-1, 0, 1, 1, -1, 1, -1, 0, 1};
+
+        visit[row][col] = true;
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{row, col});
+
+        while (!queue.isEmpty()) {
+            int point[] = queue.poll();
+            int curY = point[0];
+            int curX = point[1];
+
+            for (int i = 0; i < 8; i++) {
+                int y = curY + dy[i];
+                int x = curX + dx[i];
+                if (y < 0 || y >= visit.length || x < 0 || x >= visit[y].length) {
+                    continue;
+                }
+                if(visit[y][x]){
+                    continue;
+                }
+                if (res[y][x] == 0) {
+                    queue.add(new int[]{y, x});
+                }
+                visit[y][x] = true;
+            }
+
+        }
+
+        return visit;
+    }
+    private static int calcArea(int row, int col, char map[][]){
+        int dy[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int dx[] = {-1, 0, 1, 1, -1, 1, -1, 0, 1};
+        int cnt = 0;
+        for (int i = 0; i < 8; i++) {
+            int y = row + dy[i];
+            int x = col + dx[i];
+            if (y < 0 || y >= map.length || x < 0 || x >= map[y].length) {
+                continue;
+            }
+            if (map[y][x] == '*') {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+
+    /**
      * swea: 4193. 수영대회 결승전 ( 완전 탐색 + 구현 )
      * @throws IOException
      */
